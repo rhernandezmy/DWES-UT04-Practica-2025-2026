28bfa798f9461f88507601537c3dadbf795e9065
rhernandezmy <rhernandezmy@fpvirtualaragon.es>
2026-01-04 13:27:52 +0100

    Extender el modelo de usuario con  AbstractUser

--- gestion_de_tareas/tareas/models.py
+++ gestion_de_tareas/tareas/models.py
@@ -1,9 +1,17 @@
 import uuid
 from django.db import models
 from django.utils.text import slugify
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
+# Distinguir el tipo de usuario entre alumno y profesor
+class tipo_usuario(AbstractUser):
+    es_profesor = models.BooleanField(default=False)
+    es_alumno = models.BooleanField(default=True)
+    
+    def __str__(self):
+        return self.username
+    
 class Tarea(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     titulo = models.CharField(max_length=255)
