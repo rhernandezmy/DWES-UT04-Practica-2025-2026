0a891bc0b17c1d0c1634f1acc2bce8c77c12f5bf
rhernandezmy <rhernandezmy@fpvirtualaragon.es>
2026-01-08 20:50:29 +0100

    Vista para que los profesores vean las tareas que necesitan su validación

--- gestion_de_tareas/tareas/views.py
+++ gestion_de_tareas/tareas/views.py
@@ -77,3 +77,26 @@ def mis_tareas(request):
     }
     return render(request, 'tareas/mis_tareas.html', contexto)
     
+# Vista para que los profesores vean las tareas que necesitan su validación y superusuarios vean todas las tareas evaluables
+@login_required
+def tareas_a_validar(request):
+    usuario = request.user
+    
+    if usuario.is_superuser:
+        # Superusuario ve todas las tareas que necesitan validación
+        tareas_evaluables = TareaEvaluable.objects.filter(necesita_evaluacion=True)
+    
+    elif usuario.es_profesor:
+        # Profesores ven solo las tareas que les han sido asignadas para validar
+        tareas_evaluables = TareaEvaluable.objects.filter(profesor_validador=usuario, necesita_evaluacion=True)
+    
+    else:
+        return render(request, 'tareas/acceso_denegado.html')  # Página de acceso denegado
+    
+    # Ordenar las tareas por fecha de entrega ascendente
+    tareas_evaluables = tareas_evaluables.order_by('fecha_entrega')
+    
+    contexto = {
+        'tareas_evaluables': tareas_evaluables,
+    }
+    return render(request, 'tareas/tareas_por_validar.html', contexto)
